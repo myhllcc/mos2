b0317ab0ee284fa7c88168144fe4a499fd9b4ce4
YHM <k24104612@kcl.ac.uk>
2025-04-08 18:23:27 +0100

    add k sampling and energy of it

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from functions import *
 from scipy.constants import e as e
 from scipy.constants import hbar as hbar
 from scipy.constants import k as kb
-
+from scipy.constants import m_e as me
 def compare_phonon_number(T):
     energy = np.arange(0.1, 20, 0.1)  # avoid division by zero
     
@@ -54,37 +54,145 @@ def plot_real_space(nx_number, ny_number, a1, a2, delta_1, delta_2):
     plt.show()
 
 def plot_reciprocal_space(a, nx_number, ny_number, b1, b2):
-    delta_k1 = 2 * np.pi / (nx_number * a)
-    delta_k2 = 2 * np.pi / (ny_number * a)
-    for nx_i in np.arange(-int(nx_number / 2), int(nx_number / 2), 1):
-        for ny_i in np.arange(-int(nx_number / 2), int(ny_number / 2), 1):
+    delta_k1 = b1 / nx_number
+    delta_k2 = b2 / ny_number
+    plot_lattice_x = np.array([])
+    plot_lattice_y = np.array([])
+    for nx_i in np.arange(-1, 2, 1):
+        for ny_i in np.arange(-1, 2, 1):
             point = b1 * nx_i + b2 * ny_i
-            plt.scatter(point[0], point[1], c='blue', s = 1)
+            plt.scatter(point[0], point[1], c='blue', s = 10)
             for nx_sub_i in np.arange(0, nx_number, 1):
                 for ny_sub_i in np.arange(0, ny_number, 1):
-                    point_sub = b1 * nx_i + b2 * ny_i +  
-
+                    point_sub = b1 * nx_i + b2 * ny_i + nx_sub_i * delta_k1 + ny_sub_i * delta_k2
+                    plot_lattice_x = np.append(plot_lattice_x,point_sub[0])
+                    plot_lattice_y = np.append(plot_lattice_y,point_sub[1])
+    plt.scatter(plot_lattice_x, plot_lattice_y, c='red',s=1)
+    K = np.array([4 * np.pi / (3 * a),0])
+    plt.scatter(K[0],K[1],c='black',s=5)
+    print('finish')
     plt.axis('equal')
     plt.grid(True)
     plt.show() 
 
+def generate_sampling_near_k_1d(nx_number, ny_number, b1, b2):
+    delta_k1 = b1 / nx_number
+    delta_k2 = b2 / ny_number
+    k_array_x = np.array([])
+    k_array_y = np.array([])
+    for nx_sub_i in np.arange(0, nx_number ,1):
+        k_x = delta_k1[0] * nx_sub_i + delta_k2[0] * nx_sub_i
+        k_y = delta_k1[1] * nx_sub_i + delta_k2[1] * nx_sub_i
+        k_array_x = np.append(k_array_x, k_x)
+        k_array_y = np.append(k_array_y,k_y)
+    k_array = np.vstack((k_array_x,k_array_y))
+    return k_array
 
+def plot_band_near_k_1d(nx_number,ny_number,b1,b2,K):
+    k_array = generate_sampling_near_k_1d(nx_number,ny_number,b1,b2)
+    e_array = np.array([])
+    plt.scatter(K[0],K[1],c='black',s=5)
+    plt.scatter(k_array[0],k_array[1],c='red',s=3)
+    plt.show()
 
+    for kx in k_array[0]:
+        q = kx - K[0]
+        energy = np.square(hbar) * np.square(q) / (2 * m_eff_c) * energy_norm + energy_shift# in ev
+        e_array = np.append(e_array,energy) 
+    plt.plot(k_array[0],e_array)
+    plt.scatter(K[0],K[1],c='black',s=5)
+    plt.grid()
+    plt.xlabel('k [A^-1]')
+    plt.ylabel('energy [ev]')
+    plt.show()
 
+def generate_sampling_near_k_2d(nx_number, ny_number, b1,b2,qm): #qm is for the maximum q value
+    delta_k1 = b1 / nx_number
+    delta_k2 = b2 / ny_number
+    q_array_x = np.array([])
+    q_array_y = np.array([])
+    for nx_sub_i in np.arange(0, nx_number ,1):
+        for ny_sub_i in np.arange(0,ny_number, 1):
+            q_x = delta_k1[0] * nx_sub_i + delta_k2[0] * ny_sub_i - K[0]
+            q_y = delta_k1[1] * nx_sub_i + delta_k2[1] * ny_sub_i - K[1]
+            if (np.square(q_x) + np.square(q_y)) < qm**2:
+                q_array_x = np.append(q_array_x, q_x)
+                q_array_y = np.append(q_array_y,q_y)
+    q_array = np.vstack((q_array_x,q_array_y))
+    return q_array
 
+def plot_band_near_k_2d(nx_number, ny_number,b1,b2,energy_max):
+    qm = np.sqrt(2 * m_eff_c * energy_max / energy_norm) / hbar# in A
+    print(qm)
+    q_array = generate_sampling_near_k_2d(nx_number, ny_number, b1, b2,qm)
+    plt.scatter(q_array[0],q_array[1],s=1)
+    plt.axis('equal')
+    plt.grid()
+    plt.show()
+
+def calculate_total_number_electron(nx_number, ny_number, b1, b2, qm):
+    q_array = generate_sampling_near_k_2d(nx_number, ny_number, b1, b2, qm)
+    q_x_array = q_array[0]
+    q_y_array = q_array[1]
+    plt.scatter(q_array[0],q_array[1],s=1)
+    plt.axis('equal')
+    plt.grid()
+    plt.show()
+    
+    q_square_array = q_x_array**2 + q_y_array**2
+    energy_array = (hbar**2 * q_square_array) / (2 * m_eff_c)
+    energy_array = energy_array * energy_norm + energy_shift
+    fig = plt.figure(figsize=(10,7))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(q_x_array, q_y_array, energy_array, c=energy_array, cmap='viridis', s=20)
+    ax.view_init(elev=30, azim=135)  # Adjust view angles
+    plt.show()
+
+    
 
 kb_mev = kb / (1e-3 * e)
+kb_ev = kb / (e)
+m_eff_c = 0.43 * me
+energy_gap = 1.6
+energy_norm = 1e20 / e # J to ev  e[ev] = e_norm * e[J], also remember to use m in e[J] and A in e[ev]
+energy_shift = energy_gap / 2 # half of the band gap
+T = 100 #K
+
+
 a = 3.14 # in A
-nx_number = 10
-ny_number = 10
+nx_number = 3 * 30 * 3
+ny_number = 3 * 30 * 3
 a1 = a * np.array([0.5,np.sqrt(3) / 2])
 a2 = a * np.array([0.5,-np.sqrt(3) / 2])
 delta_1 = a * np.array([0.5,np.sqrt(3) / 6])
 delta_2 = a * np.array([0.5,-np.sqrt(3) / 6])
-
 b1 = 2 * np.sqrt(3) / (3 * a) * np.array([np.sqrt(3) / 2, 1 / 2]) * 2 * np.pi
 b2 = 2 * np.sqrt(3) / (3 * a) * np.array([np.sqrt(3) / 2, - 1 / 2]) * 2 * np.pi
 
+K = np.array([4 * np.pi / (3 * a),0]) 
+
+
+#plot_real_space(nx_number, ny_number, a1, a2, delta_1, delta_2)
+#plot_reciprocal_space(a,nx_number, ny_number, b1, b2)
+#plot_band_near_k_1d(nx_number,ny_number,b1,b2,K)
+energy_max = 1.1 #ev
+qm = np.sqrt(2 * m_eff_c * (energy_max-energy_shift) / energy_norm) / hbar# in A
+#plot_band_near_k_2d(nx_number, ny_number, b1, b2, energy_max)
+#calculate_total_number_electron(nx_number, ny_number, b1, b2, energy_max)
 print(times(a1, b1), times(a1, b2), times(a2, b1), times(a2, b2))
-plot_real_space(nx_number, ny_number, a1, a2, delta_1, delta_2)
-plot_reciprocal_space(a,nx_number, ny_number, b1, b2)
+calculate_total_number_electron(nx_number,ny_number,b1,b2,qm)
+
+rho = 3.1 * 1e-7 #g/cm^2
+rho = rho * 1e-3 * 1e4
+
+s = 1070 #m/s
+
+E_c = 2.4 #ev
+E_c = E_c * e
+
+g = m_eff_c / (2 * np.pi * np.square(hbar))
+tau_ph = hbar * rho * np.square(s) / (2 * np.pi * g * np.square(E_c) * kb * T)
+
+skew_part = g * tau_ph * 1.1 * e * tau_ph / hbar
+print(tau_ph)
+print(skew_part)
